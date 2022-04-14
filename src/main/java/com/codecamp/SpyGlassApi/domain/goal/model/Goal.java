abb70ecdc371cf6e7d82dc4ae55c71be697cfec2
Ryan Tudor <ryantudor@Ryans-Air-2.fios-router.home>
2022-04-14 15:52:22 -0400

    models complete

--- src/main/java/com/codecamp/SpyGlassApi/domain/goal/model/Goal.java
+++ src/main/java/com/codecamp/SpyGlassApi/domain/goal/model/Goal.java
@@ -1,4 +1,136 @@
 package com.codecamp.SpyGlassApi.domain.goal.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
 public class Goal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private TypeOfGoal typeOfGoal;
+    private String title;
+    private String description;
+    private String goalIcon;
+    private String customGoalColor;
+    private Double targetSavingsAmount;
+    private Double amountAlreadySaved;
+    private Date savingsDateGoal;
+    private Double progressPercentage;
+
+    public Goal() {
+    }
+
+    public Goal(TypeOfGoal typeOfGoal, String title, String description, String goalIcon, String customGoalColor, Double targetSavingsAmount, Double amountAlreadySaved, Date savingsDateGoal, Double progressPercentage) {
+        this.typeOfGoal = typeOfGoal;
+        this.title = title;
+        this.description = description;
+        this.goalIcon = goalIcon;
+        this.customGoalColor = customGoalColor;
+        this.targetSavingsAmount = targetSavingsAmount;
+        this.amountAlreadySaved = amountAlreadySaved;
+        this.savingsDateGoal = savingsDateGoal;
+        this.progressPercentage = progressPercentage;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public TypeOfGoal getTypeOfGoal() {
+        return typeOfGoal;
+    }
+
+    public void setTypeOfGoal(TypeOfGoal typeOfGoal) {
+        this.typeOfGoal = typeOfGoal;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getGoalIcon() {
+        return goalIcon;
+    }
+
+    public void setGoalIcon(String goalIcon) {
+        this.goalIcon = goalIcon;
+    }
+
+    public String getCustomGoalColor() {
+        return customGoalColor;
+    }
+
+    public void setCustomGoalColor(String customGoalColor) {
+        this.customGoalColor = customGoalColor;
+    }
+
+    public Double getTargetSavingsAmount() {
+        return targetSavingsAmount;
+    }
+
+    public void setTargetSavingsAmount(Double targetSavingsAmount) {
+        this.targetSavingsAmount = targetSavingsAmount;
+    }
+
+    public Double getAmountAlreadySaved() {
+        return amountAlreadySaved;
+    }
+
+    public void setAmountAlreadySaved(Double amountAlreadySaved) {
+        this.amountAlreadySaved = amountAlreadySaved;
+    }
+
+    public Date getSavingsDateGoal() {
+        return savingsDateGoal;
+    }
+
+    public void setSavingsDateGoal(Date savingsDateGoal) {
+        this.savingsDateGoal = savingsDateGoal;
+    }
+
+    public Double getProgressPercentage() {
+        return progressPercentage;
+    }
+
+    public void setProgressPercentage(Double progressPercentage) {
+        this.progressPercentage = progressPercentage;
+    }
+
+    @Override
+    public String toString() {
+        return "Goal{" +
+                "id=" + id +
+                ", typeOfGoal=" + typeOfGoal +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", goalIcon='" + goalIcon + '\'' +
+                ", customGoalColor='" + customGoalColor + '\'' +
+                ", targetSavingsAmount=" + targetSavingsAmount +
+                ", amountAlreadySaved=" + amountAlreadySaved +
+                ", savingsDateGoal=" + savingsDateGoal +
+                ", progressPercentage=" + progressPercentage +
+                '}';
+    }
 }
--- src/main/java/com/codecamp/SpyGlassApi/domain/user/model/User.java
+++ src/main/java/com/codecamp/SpyGlassApi/domain/user/model/User.java
@@ -1,4 +1,71 @@
 package com.codecamp.SpyGlassApi.domain.user.model;
 
+import com.codecamp.SpyGlassApi.domain.goal.model.Goal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.List;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private List<Goal> listOfGoals;
+
+    public User() {
+    }
+
+    public User(String userName, String firstName, String lastName, List<Goal> listOfGoals) {
+        this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.listOfGoals = listOfGoals;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Goal> getListOfGoals() {
+        return listOfGoals;
+    }
+
+    public void setListOfGoals(List<Goal> listOfGoals) {
+        this.listOfGoals = listOfGoals;
+    }
 }
